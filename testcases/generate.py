f168cb4cb760a9011694168bdd357f6ba5c470f9
Lars Jellema <lars.jellema@gmail.com>
2019-04-08 13:11:34 +0200

    Make tests use less space

--- testcases/generate.py
+++ testcases/generate.py
@@ -1,48 +1,82 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+alldata = bytearray()
+def data_index(datum):
+    global alldata
+    datum = bytes.fromhex(datum)
+    index = alldata.find(datum)
+    if index < 0:
+        index = len(alldata)
+        alldata += datum
+    return index
 
 def chunks(l, n):
     return [l[i:i + n] for i in range(0, len(l), n)]
 
-def hex_to_c(digits):
-    lines = chunks(chunks(digits, 2), 8)
-    lines = [' '.join('0x{},'.format(byte) for byte in data) for data in lines]
-    return '{\n' + ''.join('        ' + line + '\n' for line in lines) + '    },'
+def convert_test(lines):
+    return dict([x.strip() for x in line.split('=')]
+            for line in lines if '=' in line)
 
-def lens_to_c(vecs, i, name):
-    start = 'const unsigned long long {}_sizes[] = '.format(name) + '{\n    '
-    middle = ' '.join('{},'.format(len(vec[i]) // 2) for vec in vecs)
-    end = '\n};\n\n'
-    return start + middle + end
+def hex_to_c(data):
+    lines = chunks(data, 8)
+    lines = [' '.join('0x{:02x},'.format(byte) for byte in data) for data in lines]
+    lines = ''.join('    ' + line + '\n' for line in lines)
+    return '{\n' + lines + '}\n'
 
-def vecs_to_c(vecs, i, name):
-    sizes = ''
-    if any(len(vec[i]) != len(vecs[0][i]) for vec in vecs):
-        sizes = lens_to_c(vecs, i, name)
-    else:
-        sizes = '#define {}_SIZE {}\n'.format(name.upper(), len(vecs[0][i]) // 2)
+def fixed_sizes(tests):
+    return [field for field in tests[0]
+            if len({len(test[field]) for test in tests}) == 1]
 
-    start = 'const unsigned char {}s[][{}] = '.format(name,
-            max(len(vec[i]) for vec in vecs)) + '{\n    '
-    middle = ' '.join(hex_to_c(digits[i]) for digits in vecs)
-    end = '\n};\n'
-    return sizes + start + middle + end
+def test_struct(tests, fixed):
+    struct = '#define TESTCASE_COUNT {}\n'.format(len(tests))
+    for field in fixed:
+        struct += '#define {}_SIZE {}\n'.format(field.upper(),
+                len(tests[0][field]) // 2)
+    struct += '\nstruct testcase {\n'
+    for field in tests[0]:
+        struct += '    const unsigned char     *{};\n'.format(field)
+        if field not in fixed:
+            struct += '    const unsigned long long {}_size;\n'.format(field)
+    struct += '};\n'
+    return struct
 
-def generate(input_file, name):
+def show_test(test, fixed):
+    struct = '{\n'
+    for field, value in test.items():
+        struct += '        .{:7} = test_data + {},\n'.format(field, data_index(value))
+        if field not in fixed:
+            struct += '        .{}_size = {},\n'.format(field, len(value) // 2)
+    struct += '    },'
+    return struct
+
+def show_test_data():
+    result = 'const unsigned char test_data[] = {}'.format(hex_to_c(alldata))
+    return result + ';'
+
+def show_tests(tests, fixed):
+    result = 'const struct testcase testcases[] = {\n    '
+    result += ' '.join(show_test(test, fixed) for test in tests)
+    result += '\n};\n'
+    return result
+
+def generate(input_file):
     with open(input_file, 'r') as f:
         lines = f.readlines()
 
-    vecs = [chunk[1:7] for chunk in chunks(lines, 8)]
-    vecs = [tuple(line[10:].strip() for line in lines) for lines in vecs]
-    vecs = list(dict.fromkeys(vecs))
+    test_starts = [i for i, line in enumerate(lines) if line.startswith('####')]
+    test_ends = test_starts[1:] + [len(lines)]
+    tests = [tuple(lines[i + 1:j]) for i, j in zip(test_starts, test_ends)]
+    tests = list(dict.fromkeys(tests))
+    tests = [convert_test(test) for test in tests]
+    assert len(set(frozenset(d.keys()) for d in tests)) == 1
 
-    print('#define TESTCASE_COUNT {}\n'.format(len(vecs)))
-    print(vecs_to_c(vecs, 0, 'key'))
-    print(vecs_to_c(vecs, 1, 'pub_msg_num'))
-    print(vecs_to_c(vecs, 2, 'additional'))
-    print(vecs_to_c(vecs, 3, 'plain_text'))
-    print(vecs_to_c(vecs, 4, 'cipher_text'))
-    print(vecs_to_c(vecs, 5, 'auth_tag'))
+    fixed = fixed_sizes(tests)
+    print(test_struct(tests, fixed))
+    tests = show_tests(tests, fixed)
+    print(show_test_data())
+    print(tests)
 
 generate(*sys.argv[1:])
