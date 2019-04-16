0614abe3d778e000f5f4cef7f00552efd6cb8dd8
Lars Jellema <lars.jellema@gmail.com>
2019-04-16 15:30:39 +0200

    Begin writing introduction

--- paper/default.nix
+++ paper/default.nix
@@ -4,11 +4,14 @@ with pkgs;
 
 let
   latexrun = callPackage ./latexrun.nix {};
+  texdeps = texlive.combine {
+    inherit (texlive) scheme-basic amsmath appendix url;
+  };
 
 in stdenvNoCC.mkDerivation {
   name = "ascon-32bit.pdf";
   src = ./.;
-  nativeBuildInputs = [ latexrun texlive.combined.scheme-medium ];
+  nativeBuildInputs = [ latexrun texdeps ];
 
   buildPhase = ''
     latexrun main.tex
